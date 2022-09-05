ddd71774ab161a1bc09cdec30ec2b9ea1d3a9fe6
Juan Pablo Diaz Cervantes <53982528+jjpdc@users.noreply.github.com>
2022-09-05 15:36:50 -0500

    Update Grupo.java

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -5,7 +5,7 @@ public class Grupo {
     Persona[] estudiantes;
     Persona profesor;
     Asignatura asignatura;
-    final int codigo = 0;
+    final int codigo;
     String horario;
 
     Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
@@ -17,12 +17,13 @@ public class Grupo {
     }
 
     Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        Persona[] personas = new Persona[cantidadEstudiantes];
-        this(personas, profesor, asignatura, codigo, horario);
+    	this(new Persona[cantidadEstudiantes], profesor, asignatura, codigo, horario);
+        
     }
 
     Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
-        this.estudiantes = estudiantes;
+        this.codigo = 0;
+		this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
     }
